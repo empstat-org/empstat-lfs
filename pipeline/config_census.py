3c977c27ef663b0869c7ca8122f69e35bbd2b8b1
richardghorne <richardghorne@users.noreply.github.com>
2026-08-11 14:52:08 +0100

    name fix

--- pipeline/config_census.py
+++ pipeline/config_census.py
@@ -13,7 +13,7 @@ Run:  python fetch_and_rank.py --config config_census --out ../web/census/data
 
 from config import ILOSTAT, REGIONS  # reuse the same ILOSTAT endpoints + region map
 
-INDEX_NAME = "Population Census"
+INDEX_NAME = "Population Census Coverage Index"
 ENABLE_PENDING = False
 
 # ------------------------------------------------------------------------------
